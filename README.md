f7ec39ce7e1dd81fec2e1dabb4128d660fdf58bd
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-21 20:25:02 +0200

    Database structure migration

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ Alteris
 =======================
 
 Test kompetencji.
+
+### Baza danych
+
+Migracje wykonane za pomocą biblioteki https://github.com/vgarvardt/ZfSimpleMigrations .
+
+Wymagania to:
+* skonfigurowany adapter bazy danych (PDO MySql),
+* zftool.
+
+Migrację uruchamiamy za pomocą:
+> zf2 migration apply
+
+
--- migrations/Version20160921175335.php
+++ migrations/Version20160921175335.php
@@ -6,16 +6,41 @@ use Zend\Db\Metadata\MetadataInterface;
 
 class Version20160921175335 extends AbstractMigration
 {
-    public static $description = "Migration description";
+    public static $description = "Create the database structure";
 
     public function up(MetadataInterface $schema)
     {
-        //$this->addSql(/*Sql instruction*/);
+        $this->addSql('CREATE TABLE IF NOT EXISTS `material_group` (
+			`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+			`idParent` int(10) UNSIGNED DEFAULT NULL,
+			`name` varchar(64) NOT NULL,
+			`createdAt` timestamp NOT NULL,
+			`modifiedAt` timestamp NOT NULL,
+			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+		$this->addSql('ALTER TABLE material_group ADD CONSTRAINT fk_material_group_idParent FOREIGN KEY (idParent) '
+			. 'REFERENCES material_group(id)');
+		$this->addSql('CREATE TABLE `unit` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , '
+			. '`name` VARCHAR(64) NOT NULL , `abbreviation` VARCHAR(16) NOT NULL , '
+			. '`createdAt` TIMESTAMP NOT NULL , `modifiedAt` TIMESTAMP NOT NULL , '
+			. 'PRIMARY KEY (`id`), UNIQUE (`name`), UNIQUE (`abbreviation`)) ENGINE = InnoDB;');
+		$this->addSql('CREATE TABLE `material` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , '
+			. '`idGroup` INT UNSIGNED NOT NULL , `idUnit` INT UNSIGNED NOT NULL , `code` VARCHAR(16) NOT NULL , '
+			. '`name` VARCHAR(64) NOT NULL , `createdAt` TIMESTAMP NOT NULL , `modifiedAt` TIMESTAMP NOT NULL , '
+			. 'PRIMARY KEY (`id`), UNIQUE (`code`), UNIQUE (`name`)) ENGINE = InnoDB;');
+		$this->addSql('ALTER TABLE material ADD CONSTRAINT fk_material_idGroup FOREIGN KEY (idGroup) '
+			. 'REFERENCES material_group(id)');
+		$this->addSql('ALTER TABLE material ADD CONSTRAINT fk_material_idUnit FOREIGN KEY (idunit) '
+			. 'REFERENCES unit(id)');
     }
 
     public function down(MetadataInterface $schema)
     {
-        //throw new \RuntimeException('No way to go down!');
-        //$this->addSql(/*Sql instruction*/);
+		$this->addSql('ALTER TABLE material_group DROP FOREIGN KEY fk_material_idUnit;');
+		$this->addSql('ALTER TABLE material_group DROP FOREIGN KEY fk_material_idGroup;');
+		$this->addSql('DROP TABLE IF EXISTS `material`;');
+		$this->addSql('DROP TABLE IF EXISTS `unit`;');
+		$this->addSql('ALTER TABLE material_group DROP FOREIGN KEY fk_material_group_idParent;');
+        $this->addSql('DROP TABLE IF EXISTS `material_group`;');
     }
 }
