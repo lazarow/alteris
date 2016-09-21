d1edd1f8700c734e9321084ab3c3eb598774b9ac
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2016-09-21 19:54:29 +0200

    Create a migration

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,3 +1,14 @@
 <?php
-return array(
-);
+return [
+	'db' => [
+		'driver' => 'Pdo',
+		'driver_options' => [
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+		]
+	],
+	'service_manager' => [
+		'factories' => [
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+		]
+	]
+];
